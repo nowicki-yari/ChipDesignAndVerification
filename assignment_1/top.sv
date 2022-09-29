90e7d289ad508e3d7efefeca25601f17e3b01ac2
Yari Nowicki <yari.nowicki@outlook.com>
2022-09-29 11:21:25 +0200

    assignment 1

--- assignment_1/top.sv
+++ assignment_1/top.sv
@@ -15,16 +15,16 @@ module Top;
     );
 
     initial begin
-        theInterface.data_a <= 8'h0
-        theInterface.data_b <= 8'h0
-        theInterface.flags_in <= 4'h0
-        theInterface.operation <= 3'h0
+        theInterface.data_a <= 8'h0;
+        theInterface.data_b <= 8'h0;
+        theInterface.flags_in <= 4'h0;
+        theInterface.operation <= 3'h0;
         for (int i = 0; i <= 255; i++) begin
             repeat (10) @(posedge clock);
             theInterface.data_b += 8'b1;
         end
         repeat (1000) @(posedge clock);
-    $finish;
+        $finish;
     end
 
 endmodule
