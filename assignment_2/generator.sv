c5edb8c50fcbfab745d797487bcff60be06158e2
nowicki-yari <yari.nowicki@outlook.com>
2022-11-01 12:35:48 +0100

    assignment 2 | scoreboard

--- assignment_2/generator.sv
+++ assignment_2/generator.sv
@@ -19,7 +19,7 @@ class generator;
     s = $sformatf("[%t | GEN] I will start generating for the mailbox", $time);
     $display(s);
 
-    for(int i=0; i<100; i++)
+    for(int i=0; i<105; i++)
     begin
       tra = new();
       s = $sformatf("[%t | GEN] new instruction %s", $time, tra.toString());
