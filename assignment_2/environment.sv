dd468a89d16f7977e339106516be14a2bcd08d74
Yari Nowicki <yari.nowicki@outlook.com>
2022-11-01 09:05:25 +0100

    test generator

--- assignment_2/environment.sv
+++ assignment_2/environment.sv
@@ -1,25 +1,46 @@
+`include "transaction.sv"
+`include "generator.sv"
 `include "driver.sv"
 `include "monitor.sv"
 
 class environment;
 
+  mailbox #(transaction) gen2drv;
+
   virtual gb_iface ifc;
 
+  generator gen;
   driver drv;
   monitor mon;
 
   function new(virtual gb_iface ifc);
-    this.drv = new(ifc);
+    this.ifc = ifc;
+
+    this.gen2drv = new(5);
+
+    this.gen = new(this.gen2drv);
+    this.drv = new(ifc, this.gen2drv);
     this.mon = new(ifc);
   endfunction : new
 
   task run();
+    string s;
+
+    $timeformat(-9,0," ns" , 10);
+
+    s = $sformatf("[%t | ENV] I will set up the components", $time);
+    $display(s);
+
+    this.drv.do_reset();
+
     fork
       this.drv.run_addition();
       this.mon.run();
+      this.gen.run();
     join_any;
 
-    $display("[ENV]: end of run()");
+    s = $sformatf("[%t | ENV]  end of run()", $time);
+    $display(s);
 
   endtask : run
 
--- assignment_2/generator.sv
+++ assignment_2/generator.sv
@@ -2,17 +2,27 @@
 
 class generator;
 
-  function new;
+  mailbox #(transaction) gen2drv;
 
+  function new(mailbox #(transaction) g2d);
+    this.gen2drv = g2d;
   endfunction : new
 
   task run;
+    string s;
     transaction tra;
+
+    $timeformat(-9,0," ns" , 10);
+
+    s = $sformatf("[%t | GEN] I will start generating for the mailbox", $time);
+    $display(s);
 
     for(int i=0; i<10; i++)
     begin
       tra = new();
-      $display("%s", tra.toString());
+      s = $sformatf("[%t | GEN] new instruction %s", $time, tra.toString());
+      $display(s);
+      this.gen2drv.put(tra);
     end
   endtask : run
 
