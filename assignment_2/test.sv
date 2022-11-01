562d94a134edc75f8533550ba263d3819b561924
Yari Nowicki <yari.nowicki@outlook.com>
2022-11-01 09:13:44 +0100

    test generator

--- assignment_2/test.sv
+++ assignment_2/test.sv
@@ -1,25 +1,13 @@
-/*
 `include "gb_iface.sv"
 `include "environment.sv"
 
 program test (gb_iface ifc);
-*/
-`include "generator.sv"
-program test();
-  /*
+
   environment env = new(ifc);
 
   initial
   begin
     env.run();
-  end
-  */
-
-  generator gen = new();
-
-  initial
-  begin
-    gen.run();
   end
 
 endprogram : test
