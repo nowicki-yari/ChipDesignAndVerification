5f25ebe06e1db1202694475bdae8853d780a7c7d
Yari Nowicki <yari.nowicki@outlook.com>
2022-11-15 15:01:51 +0100

    a5

--- assignment_5/scoreboard.sv
+++ assignment_5/scoreboard.sv
@@ -29,7 +29,7 @@ class scoreboard;
                 incorrect_tests += 1;
             end
         end
-        $display($get_coverage())
+        $display($get_coverage());
     endtask : score
 
 
