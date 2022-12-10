6607c36ddcf13a93099c573285d74d9e070f176b
nowicki-yari <yari.nowicki@outlook.com>
2022-12-10 13:46:06 +0100

    ADC

--- Project/gameboyprocessor.sv
+++ Project/gameboyprocessor.sv
@@ -103,42 +103,43 @@ class gameboyprocessor;
             begin
                 if(instr[2:0] == 3'b000) // B
                 begin
+                    
                     carry = computeCarry(this.B, 1'b0); 
-                    this.A = this.A + this.B + carry[7];
+                    this.A = this.A + this.B + this.F[4];
                     
                 end else if(instr[2:0] == 3'b001) // C
                 begin
                      carry = computeCarry(this.C, 1'b0); 
-                    this.A = this.A + this.C + carry[7];
+                    this.A = this.A + this.C + this.F[4];
                    
                 end else if(instr[2:0] == 3'b010) // D
                 begin
                     carry = computeCarry(this.D, 1'b0); 
-                    this.A = this.A + this.D + carry[7];
+                    this.A = this.A + this.D + this.F[4];
                     
                 end else if(instr[2:0] == 3'b011) // E
                 begin
                     carry = computeCarry(this.E, 1'b0); 
-                    this.A = this.A + this.E + carry[7];
+                    this.A = this.A + this.E + this.F[4];
                     
                 end else if(instr[2:0] == 3'b100) // H
                 begin
                     carry = computeCarry(this.H, 1'b0); 
-                    this.A = this.A + this.H + carry[7];
+                    this.A = this.A + this.H + this.F[4];
                     
                 end else if (instr[2:0] == 3'b101) // L
                 begin 
                     carry = computeCarry(this.L, 1'b0); 
-                    this.A = this.A + this.L + carry[7];
+                    this.A = this.A + this.L + this.F[4];
                    
                 end else if (instr[2:0] == 3'b110) // HL
                 begin
                     carry = computeCarry(8'h00, 1'b0); 
-                    this.A = this.A + carry[7];
+                    this.A = this.A + this.F[4];
                     
                 end else begin // A
                     carry = computeCarry(this.A, 1'b0); 
-                    this.A = this.A + this.A + carry[7];
+                    this.A = this.A + this.A + this.F[4];
                     
                 end
                 if (this.A == 0)
